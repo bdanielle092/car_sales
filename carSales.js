157a6f2d7b8e7df513672798fa5cea37d3a99894
Faith Magras <bdanielle092@gmail.com>
2020-06-24 23:46:50 -0500

    finish outputing to the dom

---
+++ carSales.js
@@ -0,0 +1,18 @@
+import salesByWeek from "./data.js"
+// weeklySales is gets info from HTML
+const weeklySales = document.querySelector(".report")
+// this is the header
+ weeklySales.innerHTML += "<h1>Weekly Sales Reports</h1>"
+// is a loop running through the data
+ salesByWeek.forEach(sale => {
+    //  this is putting first and last name from the data
+    weeklySales.innerHTML += `<hr><h2>${sale.sales_agent.first_name} ${sale.sales_agent.last_name}</h2></hr>`
+    // this is another loop and is pulling the properties
+        for (const property of Object.entries(sale.vehicle)) {
+            // this tells where the properties are in HTML. 0 is name and 1 is vechicle properties
+             weeklySales.innerHTML += `<div>${property[0]}: ${property[1]}</div>`
+     }
+    //    this is pulling the info on profit from data. There is an extra $ sign so it shows before profit
+        weeklySales.innerHTML += `<h3>Profit: $${sale.gross_profit}</h3>`
+ })
+
